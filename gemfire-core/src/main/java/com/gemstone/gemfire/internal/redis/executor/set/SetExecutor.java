d12253dcefb37bec132854446faf62c43d258e15
Vito Gavrilov <vgavrilov@pivotal.io>
2015-08-06 18:25:51 -0700

    Fix build.grade in core and cleanup

--- gemfire-core/src/main/java/com/gemstone/gemfire/internal/redis/executor/set/SetExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.gemstone.gemfire.internal.redis.executor.set;
-
-import com.gemstone.gemfire.internal.redis.executor.AbstractExecutor;
-
-public abstract class SetExecutor extends AbstractExecutor {
-
-}
--- gemfire-core/src/main/java/com/gemstone/gemfire/internal/redis/executor/transactions/TransactionExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.gemstone.gemfire.internal.redis.executor.transactions;
-
-import com.gemstone.gemfire.internal.redis.executor.AbstractExecutor;
-
-public abstract class TransactionExecutor extends AbstractExecutor {
-
-}
